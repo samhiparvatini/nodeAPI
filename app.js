7f4f91e063b40f557acce6a08d260cead3b59952
Samhita Parvatini <samhiparvatini@gmail.com>
2024-07-19 10:36:48 -0400

    modified parser and POST for users

--- app.js
+++ app.js
@@ -5,19 +5,36 @@
 //The assignment to variable express is to make the functionality available within the application.
 //The variable can be used to create an instance of an Express application and use its features to define routes, handle HTTP requests, and more
 const express = require('express');
+//const bodyParser = require('body-parser');
+const router = require('./router');
+
 // Creates a new express application. 
 // This application is a JavaScript object that provides a set of methods and properties to define routes, handle HTTP requests, set up middleware, and configure your server.
 // Variable app is used as reference to the new Javascript object/Express application
 const app = express();
-const router = require('./router');
+app.use(express.json());
 
-//use the router for the defined routes in router.js
-app.use('/api', router)
-
+/*app.all('*', async(req,res,next) => {
+    // app.set('response', undefined);
+    // req.appObj = this.app;
+    // console.log(req.appObj);
+    next();
+});*/
 //starting the server
 //sets the port number that the Express.js server will listen on
 //process.env.PORT retrieves the environment variable PORT
 const port = process.env.PORT || 3000; //default to 3000
+//app.use(bodyParser.json)
+//console.log('body_parser');
+
+//use the router for the defined routes in router.js
+app.use('/api', router);
+
+/*app.use((req,res,next) => {
+    // res.json(req.appObj.get('response'));
+    res.end();
+});*/
+
 
 //starts the Express.js server and tells the server to listen for the incoming HTTP requests on the specified port
 //app.listen(port, () => {...} is used to start the server. 'port' is the port number that the server should listen on (either PORT env variable or default to 3000). 
@@ -25,4 +42,6 @@ const port = process.env.PORT || 3000; //default to 3000
 // console.log() is used to log a message to the console as a indicator
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
-})
+});
+
+
--- router.js
+++ router.js
@@ -4,10 +4,8 @@
 //import handlers
 
 const express = require('express');
-
 //creates a new instance of Express Router 
 const router = express.Router();
-
 const Sequelize = require('sequelize');
 const config = require('./config.json')['development'];
 
@@ -19,11 +17,13 @@ const sequelize = new Sequelize(config.database, config.username, config.passwor
 
 sequelize.authenticate() 
     .then(() => {
-        console.log('Connection has been established successfully.');
+        console.log(`Connection has been established successfully to ${config.host} in database ${config.database}`);
     })
     .catch(err => {
-        console.error('Unable to connect to the database:', err);
+        console.error(`Unable to connect to the database:`, err);
     });
+
+console.log('connection');
 //define routes
 
 //defining a route handler to handle GET requests to the specified endpoint of the Express application
@@ -40,8 +40,61 @@ router.get('/users', (req, res) => {
 //app.post(endpoint, ...) tells Express to handle POST requests to this URL path.
 //(req, res) => {...} is the callback function that gets executed when a POST request is made to the endpoint. req is short for request and res is short for response.
 //res.json({message: ...}) is for inside the callback function, where it sends a JSON response back to the client with a message as an indicator that the request has been performed.
-router.post('/users', (req, res) => {
-    res.send('Create a new user');
+router.post('/users', async(req, res) => {
+    console.log("received POST request");
+    console.log("request body:", req.body);
+    const {username, first_name, last_name, password, role_name} = req.body;
+    try {
+        const transaction = await sequelize.transaction()
+
+        try{
+            //insert into usersrole table
+            const createUserRoleQuery = `
+            INSERT INTO usersrole (role_name, created_at, updated_at) 
+            VALUES ($role_name, NOW(), NOW())
+            RETURNING role_id`;
+
+            let bind = {'role_name': req.body.role_name}
+
+            const [newUserRole, userRoleMetadata] = await sequelize.query(createUserRoleQuery, {
+                transaction,
+                bind,
+                type: Sequelize.QueryTypes.INSERT
+            });
+
+            const role_id = newUserRole[0].role_id;
+            console.log(role_id);
+            //insert into users table
+            const createUserQuery = `
+            INSERT INTO users (username,first_name, last_name, password, role_id, created_at, updated_at) 
+            VALUES ($username, $first_name, $last_name, $password, $role_id, NOW(), NOW())
+            RETURNING *`;
+
+            bind = {'username': req.body.username, 'first_name': req.body.first_name, 'last_name':req.body.last_name, 'password': req.body.password, 'role_id':role_id}
+            const [newUser, userMetadata] = await sequelize.query(createUserQuery, {
+                transaction,
+                bind,
+                type: Sequelize.QueryTypes.INSERT
+            });
+
+            //commit transaction if everything is successful
+            await transaction.commit();
+
+            console.log("user created successfuly:", newUser);
+            await res.status(201).json({message: 'User successfully created', user: newUser});
+        } catch (error) {
+            await transaction.rollback();
+            console.log(error);
+            throw error;
+        }
+    } catch (error) {
+        console.error('Error creating user and role:', error);
+        res.status(500).json({error: 'Failed to create user and role'});
+    }
+});
+
+router.put('/users', (req, res) => {
+    res.send('Create a new post');
 });
 
 router.get('/profile', (req, res) => {
@@ -56,9 +109,7 @@ router.get('/posts', (req, res) => {
     res.send('Get all posts');
 });
 
-router.post('/users', (req, res) => {
-    res.send('Create a new post');
-});
+
 
 router.get('/messages', (req, res) => {
     res.send('Show all messages');
@@ -68,10 +119,18 @@ router.post('/messages', (req, res) => {
     res.send('Start a new message');
 });
 
-router.get('/testsequelize', (req, res) => {
+router.get('/testsequelize', (req, res, next) => {
     res.send(sequelize.query('SELECT * FROM users', {type: Sequelize.QueryTypes.SELECT})
     .then(users => {
         console.log(users);
+        // res.set('result', users);
+        //res.json(users);
+        // console.log(res);
+        // console.log(req.appObj);
+        // req.appObj.set('response', users);
+        next();
+        //res.json(res);
+        //console.log(users);
       })
       .catch(err => {
         console.error('Error executing query:', err);
